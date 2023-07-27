165d122b0fe516f69433b9160758ae129c4c9d73
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-07-27 09:12:03 +0200

    Fixed UI bugs

--- src/modules/static.js
+++ src/modules/static.js
@@ -88,6 +88,7 @@ export function dayGridType(day) {
             output.available = false;
             output.partialAvailability = true;
           } else if (!dayObject.available && !dayObject.partialAvailability) {
+            console.log(dayObject.day);
             output.available = false;
             output.partialAvailability = false;
           }
