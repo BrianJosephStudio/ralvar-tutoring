5e30841ccef2665c12de2abe4c388d45cc869782
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-08-21 19:07:29 +0200

    Progress Payment View

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="payment">
-        <h1>Payment Page under development</h1>
+        <form id="payment-form" class="form">
+            <div id="link-authentication-element">
+                <!--Stripe.js injects the Link Authentication Element-->
+            </div>
+            <div id="payment-element">
+                <!--Stripe.js injects the Payment Element-->
+            </div>
+            <button id="submit">
+                <div class="spinner hidden" id="spinner"></div>
+                <span id="button-text">Pay now</span>
+            </button>
+            <div id="payment-message" class="hidden"></div>
+        </form>
     </div>
 </template>
 
@@ -8,10 +20,52 @@
 import { onMounted } from "vue"
 import { useStore } from "vuex";
 import { useRouter } from 'vue-router'
+import { loadStripe } from "@stripe/stripe-js"
+
 
 const store = useStore();
 const router = useRouter();
 
+onMounted(async () => {
+    const stripe = await loadStripe(`pk_test_51NgmsQICkqKXp7Quz3Pg96iYp2kMrCDzTv2haJP322fpyrJOQJBWL8WrZexBzfeNnNSQgfqoMqKl8tS9TT1Yv1ip00ZMzwmQzu`)
+    let elements
+    const response = await fetch("http://localhost:3000/api/paymentIntent", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            amount: store.state.bookings.booking.paymentData.checkoutPrice
+        })
+    })
+    if (!response.ok) {
+        console.log(await response.json())
+        return
+    }
+    const { clientSecret } = await response.json()
+    console.log("aqui")
+    console.log(clientSecret)
+    const appearance = {
+        theme: "stripe",
+    }
+
+    elements = stripe.elements({ appearance, clientSecret })
+    let emailAddress = ""
+    const linkAuthenticationElement = elements.create("linkAuthentication");
+    // linkAuthenticationElement.mount("#link-authentication-element");
+
+    // linkAuthenticationElement.on('change', (event) => {
+    //     emailAddress = event.value.email;
+    // });
+
+    const paymentElementOptions = {
+        layout: "accordion",
+    };
+
+    const paymentElement = elements.create("payment", paymentElementOptions);
+    paymentElement.mount("#payment-element");
+    return
+
+
+})
 
 
 </script>
@@ -30,5 +84,20 @@ const router = useRouter();
         padding: 0;
         font-size: 3vw;
     }
+
+    .form {
+        display: flex;
+        // flex-direction: "column";
+        justify-content: center;
+        // width: 100%;
+        // height: 50vh;
+
+
+        .input {
+            width: 500px;
+            height: 30px;
+        }
+    }
+
 }
 </style>
