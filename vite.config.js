2e6fddd795bd389f95a10ea0de7a4a0b60dfc040
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-07 23:11:41 +0200

    Vite outDir path changed for Ubuntu path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: "../rachel-server-windows/dist",
+    outDir: "../server/dist",
     sourcemap: true,
   },
   optimizeDeps: {
