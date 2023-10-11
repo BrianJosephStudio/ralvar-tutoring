8003e50994367dc605d23d105f1838dbd09a249e
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-11 22:25:29 +0200

    Return URL for Stripe Updated

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const domain = "localhost";
+const domain = "ralvar.com";
 const port = 3000;
-const url = `http://${domain}:${port}`;
+const url = `http://${domain}`;
 const api = `/api`;
 export default {
   domain,
