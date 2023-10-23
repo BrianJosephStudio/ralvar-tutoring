695c46a2e7e2df977f9ca55ebef1e06aa21175c6
Brian Joseph Studio <brianjosephstudio@gmail.com>
2023-10-23 23:34:32 +0200

    Added Public folder to gitignore

--- public/assets/Classes-2a163856.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o,c as s}from"./index-8d469eb3.js";const t={},c={style:{color:"black"}};function r(a,n,_,l,p,f){return o(),s("h1",c,"hey")}const i=e(t,[["render",r]]);export{i as default};
-//# sourceMappingURL=Classes-2a163856.js.map
--- public/assets/TabNotFound-cbf78bfb.js
+++
@@ -1,2 +0,0 @@
-import{_ as c,e as n,f as _,o as r,c as u,a as t,q as o,t as l,F as p,l as d,m as f}from"./index-8d469eb3.js";const h=e=>(d("data-v-4a5cf592"),e=e(),f(),e),i=h(()=>t("h2",null,"404 - Tab not found",-1)),m={__name:"TabNotFound",setup(e){const s=n(),a=_("");return a.value=s.currentRoute.value.path,(v,x)=>(r(),u(p,null,[i,t("h1",null,[o(`We're sorry! The tab "`),t("span",null,l(a.value.slice(1)),1),o('" does not exist.')])],64))}},b=c(m,[["__scopeId","data-v-4a5cf592"]]);export{b as default};
-//# sourceMappingURL=TabNotFound-cbf78bfb.js.map
